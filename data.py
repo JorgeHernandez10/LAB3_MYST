b468d5e04c94f71ed183dda233414efca0c43fb3
DayanaNavarro <54543172+DayanaNavarro@users.noreply.github.com>
2022-10-10 14:09:59 -0500

    Z

---
+++ data.py
@@ -0,0 +1,18 @@
+
+"""
+# -- --------------------------------------------------------------------------------------------------- -- #
+# -- project: A SHORT DESCRIPTION OF THE PROJECT                                                         -- #
+# -- script: data.py : python script for data collection                                                 -- #
+# -- author: YOUR GITHUB USER NAME                                                                       -- #
+# -- license: THE LICENSE TYPE AS STATED IN THE REPOSITORY                                               -- #
+# -- repository: YOUR REPOSITORY URL                                                                     -- #
+# -- --------------------------------------------------------------------------------------------------- -- #
+"""
+
+dict_test = {'key_a': 'a', 'key_b': 'b'}
+
+# Prueba
+print("a ver si se hace el cambio de los files")
+
+# branch sebas
+# aquí va a trabajar sebas 
